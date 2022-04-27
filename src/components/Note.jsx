f053b610273ca79673a2ed8ab34a6b2d9d7cfe0b
Ireaneus <bwdave@protonmail.com>
2022-04-27 15:59:52 -0600

    Almost completed on my own Using exercises before, but that is not cheating it is using already stuff

---
+++ src/components/Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div class="note">
+      <h1 class="note">This is a title of note</h1>
+      <p class="note">This is the body of note</p>
+    </div>
+  );
+}
+
+export default Note;
