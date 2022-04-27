610516977cc0c46497208b38c5d067028243c541
Ireaneus <bwdave@protonmail.com>
2022-04-27 16:13:52 -0600

    date and className

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Footer() {
   return (
     <footer>
-      <p>Copyright today's year here</p>
+      <p>Copyright ⓒ {new Date().getFullYear()}</p>
     </footer>
   );
 }
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 function Note() {
   return (
-    <div class="note">
-      <h1 class="note">This is a title of note</h1>
-      <p class="note">This is the body of note</p>
+    <div className="note">
+      <h1>This is a title of note</h1>
+      <p>This is the body of note</p>
     </div>
   );
 }
